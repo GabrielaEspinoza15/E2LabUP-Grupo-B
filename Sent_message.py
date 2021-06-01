3b7c28cff70051c1c6d37bc9eb9b36bc8f9ba9f9
GabrielaEspinoza15 <83046778+GabrielaEspinoza15@users.noreply.github.com>
2021-06-01 18:00:15 -0500

    Update Sent_message.py

--- Sent_message.py
+++ Sent_message.py
@@ -3,6 +3,8 @@ from Google import Create_Service
 import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import docx2txt
+
 
 #Archivo descargado desde Google Cloud Platform
 CLIENT_SECRET_FILE = 'client_secret.json'
@@ -14,9 +16,10 @@ service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
 
 #Contenido del correo
-emailMsg = ":')"
+emailMsg = docx2txt.process(r'''C:\Users\Usuario\Documents\GMAIL_API\Archivo\Mensaje.docx''')
+
 mimeMessage = MIMEMultipart()
-mimeMessage['to'] = '<gabrielamylen15@gmail.com>'
+mimeMessage['to'] = '<gabrielamylen15@gmail.com>, <gabriela.espinoza@aiesec.net>'
 mimeMessage['subject'] = 'Mensaje enviado desde Python'
 mimeMessage.attach(MIMEText(emailMsg, 'plain'))
 raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
