96df2aaa2665fc70ac5979c2ceb68411a1cff2c4
GabrielaEspinoza15 <83046778+GabrielaEspinoza15@users.noreply.github.com>
2021-06-01 16:42:33 -0500

    Create Sent_message.py

---
+++ Sent_message.py
@@ -0,0 +1,25 @@
+#Google creado con Google.py
+from Google import Create_Service
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#Archivo descargado desde Google Cloud Platform
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_NAME = 'gmail'
+API_VERSION = 'v1'
+SCOPES = ['https://mail.google.com/']
+
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+
+#Contenido del correo
+emailMsg = ":')"
+mimeMessage = MIMEMultipart()
+mimeMessage['to'] = '<gabrielamylen15@gmail.com>'
+mimeMessage['subject'] = 'Mensaje enviado desde Python'
+mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+
+message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+print(message)
