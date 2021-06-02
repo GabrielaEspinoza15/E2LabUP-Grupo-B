337e41450394854ab5c4eb6eedefd018a213fca9
GabrielaEspinoza15 <83046778+GabrielaEspinoza15@users.noreply.github.com>
2021-06-01 20:22:44 -0500

    Update Sent_message.py

--- Sent_message.py
+++ Sent_message.py
@@ -4,9 +4,10 @@ import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import docx2txt
+import schedule
+import time
 
 
-#Archivo descargado desde Google Cloud Platform
 CLIENT_SECRET_FILE = 'client_secret.json'
 API_NAME = 'gmail'
 API_VERSION = 'v1'
@@ -14,15 +15,29 @@ SCOPES = ['https://mail.google.com/']
 
 service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
 
+running = True
+def test():
 
-#Contenido del correo
-emailMsg = docx2txt.process(r'''C:\Users\Usuario\Documents\GMAIL_API\Archivo\Mensaje.docx''')
 
-mimeMessage = MIMEMultipart()
-mimeMessage['to'] = '<gabrielamylen15@gmail.com>, <gabriela.espinoza@aiesec.net>'
-mimeMessage['subject'] = 'Mensaje enviado desde Python'
-mimeMessage.attach(MIMEText(emailMsg, 'plain'))
-raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+    emailMsg = docx2txt.process(r'''C:\Users\Usuario\Documents\GMAIL_API\Archivo\Mensaje.docx''')
 
-message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
-print(message)
+    mimeMessage = MIMEMultipart()
+    mimeMessage['to'] = '<gabrielamylen15@gmail.com>, <gabriela.espinoza@aiesec.net>'
+    mimeMessage['subject'] = 'Mensaje enviado desde Python'
+    mimeMessage.attach(MIMEText(emailMsg, 'plain'))
+    raw_string = base64.urlsafe_b64encode(mimeMessage.as_bytes()).decode()
+
+    message = service.users().messages().send(userId='me', body={'raw': raw_string}).execute()
+    
+    global running
+    running = False
+
+    print(message)
+
+
+    return schedule.CancelJob
+#Hora programada
+schedule.every().day.at("20:19").do(test)
+while running:
+    schedule.run_pending()
+    time.sleep(1)
